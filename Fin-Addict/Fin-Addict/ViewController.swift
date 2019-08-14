308608f8f0b0f6585857996c216b10d9327c6290
Emmy <EmmyChavez@Apples-MacBook-Pro-47.local>
2019-08-14 11:46:53 -0400

    Welcome Page

---
+++ Fin-Addict/Fin-Addict/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fin-Addict
+//
+//  Created by Emmy on 8/14/19.
+//  Copyright © 2019 Emmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fin-Addict/Fin-AddictTests/Fin_AddictTests.swift
@@ -0,0 +1,34 @@
+//
+//  Fin_AddictTests.swift
+//  Fin-AddictTests
+//
+//  Created by Emmy on 8/14/19.
+//  Copyright © 2019 Emmy. All rights reserved.
+//
+
+import XCTest
+@testable import Fin_Addict
+
+class Fin_AddictTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
